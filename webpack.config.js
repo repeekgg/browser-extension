74430eb8d69695e6c105aa5309e6e13e03169a59
Tim Cheung <tim@cheung.io>
2018-03-06 20:10:50 +0100

    disable uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+  devtool: 'sourcemap',
   context: path.resolve(__dirname, 'extension', 'src'),
   entry: {
     content: './content/index.js',
@@ -10,13 +12,28 @@ module.exports = {
     path: path.resolve(__dirname, 'extension', 'dist'),
     filename: '[name].js'
   },
-  "module": {
-    "rules": [
+  module: {
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
       }
+    ]
+  },
+  optimization: {
+    concatenateModules: true,
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          mangle: false,
+          compress: false,
+          output: {
+            beautify: true,
+            indent_level: 2
+          }
+        }
+      })
     ]
   }
 }
