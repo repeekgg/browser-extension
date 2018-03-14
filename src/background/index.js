e5bd9ded47f2ebcf0c3add8c8e8f6faba1b3c963
Tim Cheung <tim@cheung.io>
2018-03-14 14:37:18 +0100

    stop open changelog in development

--- src/background/index.js
+++ src/background/index.js
@@ -1,7 +1,15 @@
 import changelog from './changelog'
 
-chrome.runtime.onInstalled.addListener(({ reason }) => {
+chrome.runtime.onInstalled.addListener(async ({ reason }) => {
   if (reason === 'update') {
+    const { installType } = await new Promise(resolve => {
+      chrome.management.getSelf(resolve)
+    })
+
+    if (installType === 'development') {
+      return
+    }
+
     const { version } = chrome.runtime.getManifest()
     const changelogUrl = changelog[version]
 
