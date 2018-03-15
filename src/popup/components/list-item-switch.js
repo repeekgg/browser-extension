b1d04d022390aac42c9fd5741fc6fc1b67a8e745
Tim Cheung <tim@cheung.io>
2018-03-15 11:16:05 +0100

    improve layout

--- src/popup/components/list-item-switch.js
+++ src/popup/components/list-item-switch.js
@@ -8,7 +8,7 @@ import Switch from 'material-ui/Switch'
 
 export default ({ onClick, checked, ...props }) => (
   <ListItem onClick={onClick} button>
-    <ListItemText {...props} />
+    <ListItemText {...props} style={{ paddingRight: 60 }} />
     <ListItemSecondaryAction>
       <Switch onChange={onClick} checked={checked} />
     </ListItemSecondaryAction>
