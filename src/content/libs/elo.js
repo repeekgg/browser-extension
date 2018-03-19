15dd34d505991ec7220b6eb07fc2a567ba94570d
James D <clugcs@gmail.com>
2018-03-19 23:18:11 +1100

    set elo minimum to +- 10 to copy faceit's system

--- src/content/libs/elo.js
+++ src/content/libs/elo.js
@@ -5,8 +5,8 @@ export function calculateRatingChange(elo1, elo2, K = 50) {
   const eloDiff = elo2 - elo1
   const percentage = 1 / (1 + Math.pow(10, eloDiff / 400))
 
-  const winPoints = Math.round(K * (1 - percentage))
-  const lossPoints = Math.round(K * (0 - percentage))
+  const winPoints = Math.max(10, Math.round(K * (1 - percentage)))
+  const lossPoints = Math.max(10, Math.round(K * (0 - percentage)))
 
   return {
     winPoints,
