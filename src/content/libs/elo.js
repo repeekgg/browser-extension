6a722eed878b29b739bf5bee730a2d405c3889f3
Tim Cheung <tim@cheung.io>
2018-03-22 21:28:08 +0100

    gain or lose min. 10 points only if 0 points

--- src/content/libs/elo.js
+++ src/content/libs/elo.js
@@ -1,17 +1,16 @@
 /* eslint-disable import/prefer-default-export */
-import clamp from 'lodash.clamp'
 import mem from 'mem'
 
 export function calculateRatingChange(elo1, elo2, K = 50) {
   const eloDiff = elo2 - elo1
   const percentage = 1 / (1 + Math.pow(10, eloDiff / 400))
 
-  const winPoints = clamp(Math.round(K * (1 - percentage)), 10, 50)
-  const lossPoints = clamp(Math.round(K * (0 - percentage)), -50, -10)
+  const winPoints = Math.round(K * (1 - percentage))
+  const lossPoints = Math.round(K * (0 - percentage))
 
   return {
-    winPoints,
-    lossPoints
+    winPoints: winPoints === 0 ? 10 : winPoints,
+    lossPoints: lossPoints === 0 ? -10 : lossPoints
   }
 }
 
