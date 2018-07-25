4a428c21a1df1d53372b3d09ae12d10a7a1e363d
Daniel Skogly <daniel.skogly@protonmail.com>
2018-07-25 16:59:57 +0200

    Update skill level icon to use new SVGs

--- src/content/components/skill-level.js
+++ src/content/components/skill-level.js
@@ -1,10 +1,12 @@
 /** @jsx h */
 import { h } from 'dom-chef'
 
-export default ({ level, size = 'md', style = {} }) => (
+export default ({ level, size = 32, style = {} }) => (
   <img
     alt={`Skill Level ${level}`}
-    src={`https://cdn.faceit.com/frontend/614/assets/images/skill-icons/skill_level_${level}_${size}.png`}
+    height={size}
+    src={`https://cdn-frontend.faceit.com/web/960/src/app/assets/images-compress/skill-icons/skill_level_${level}_svg.svg`}
     style={style}
+    width={size}
   />
 )
