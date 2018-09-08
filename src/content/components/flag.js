225926870bab656e5bffeb63aeed6a0b8822fe30
Daniel Skogly <daniel.skogly@protonmail.com>
2018-09-08 10:19:51 +0200

    Update flag image base URL

--- src/content/components/flag.js
+++ src/content/components/flag.js
@@ -3,7 +3,7 @@ import { h } from 'dom-chef'
 
 export default ({ country, alignRight = false }) => (
   <img
-    src={`https://cdn.faceit.com/frontend/561/assets/images/flags/${country.toUpperCase()}.png`}
+    src={`https://cdn-frontend.faceit.com/web/112-1536332382/src/app/assets/images-compress/flags/${country.toUpperCase()}.png`}
     className="flag--12"
     style={{
       [`margin-${alignRight ? 'left' : 'right'}`]: 6,
