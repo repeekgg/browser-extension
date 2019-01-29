069f66af25db89d38cfd8a4642854c0b672c7b36
Tim Cheung <tim@cheung.io>
2019-01-29 17:13:47 +0000

    show title on country flag

--- src/content/components/flag.js
+++ src/content/components/flag.js
@@ -1,13 +1,21 @@
 /** @jsx h */
 import { h } from 'dom-chef'
+import { getName } from 'country-list'
 
-export default ({ country, alignRight = false }) => (
-  <img
-    src={`https://cdn-frontend.faceit.com/web/112-1536332382/src/app/assets/images-compress/flags/${country.toUpperCase()}.png`}
-    className="flag--12"
-    style={{
-      [`margin-${alignRight ? 'left' : 'right'}`]: 6,
-      'vertical-align': 'unset'
-    }}
-  />
-)
+export default ({ country, alignRight = false }) => {
+  const countryCode = country.toUpperCase()
+  const countryName = getName(countryCode) || ''
+
+  return (
+    <img
+      src={`https://cdn-frontend.faceit.com/web/112-1536332382/src/app/assets/images-compress/flags/${countryCode}.png`}
+      className="flag--12"
+      style={{
+        [`margin-${alignRight ? 'left' : 'right'}`]: 6,
+        'vertical-align': 'unset'
+      }}
+      alt={countryName}
+      title={countryName}
+    />
+  )
+}
