1a9b8b0f8dc462c909c458afae74dd5c760bdb31
Daniel Skogly <daniel.skogly@protonmail.com>
2019-02-09 19:18:29 +0100

    Update getPlayerProfileNickname regex to handle more names

--- src/content/libs/player-profile.js
+++ src/content/libs/player-profile.js
@@ -2,9 +2,7 @@
 import { getCurrentPath } from './location'
 
 export const getPlayerProfileNickname = path => {
-  const match = /players(?:-modal)?\/([a-zA-Z0-9_-]+)\//.exec(
-    path || getCurrentPath()
-  )
+  const match = /players(?:-modal)?\/([^/]+)\//.exec(path || getCurrentPath())
 
   return match && match[1]
 }
