2dcb84545c965f54c7bcef521bcc10dea033b284
Tim Cheung <tim@cheung.io>
2019-05-06 19:41:10 +0100

    fix v2.0.0 changelog

--- src/background/index.js
+++ src/background/index.js
@@ -66,7 +66,16 @@ browser.runtime.onInstalled.addListener(async ({ reason, previousVersion }) => {
       return
     }
 
-    const changelogUrl = changelogs[version]
+    let changelogUrl = changelogs[version]
+
+    // @TODO: Remove later
+    if (
+      !['2.0.0', '2.1.0', '2.2.0', '2.3.0', '2.4.0', '2.5.0'].includes(
+        previousVersion
+      )
+    ) {
+      changelogUrl = 'https://redd.it/bkx7tl'
+    }
 
     if (changelogUrl) {
       const {
