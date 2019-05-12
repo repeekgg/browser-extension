97d0799cc4ed865e95fb26672c283ba43f83c4a9
Tim Cheung <tim@cheung.io>
2019-05-12 23:30:51 +0100

    show help cursor on badge hover

--- src/content/components/player-badge.js
+++ src/content/components/player-badge.js
@@ -4,7 +4,7 @@ import { h } from 'dom-chef'
 export default ({ role, bgColor, textColor, description = '' }) => (
   <span
     className="label label-primary"
-    style={{ 'background-color': bgColor, color: textColor }}
+    style={{ 'background-color': bgColor, color: textColor, cursor: 'help' }}
     title={description}
   >
     FACEIT Enhancer {role}
