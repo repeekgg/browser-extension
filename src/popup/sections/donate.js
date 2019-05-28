c07193a81b1456da0a4b55f5ab248d810ec75a27
Tim Cheung <tim@cheung.io>
2019-05-28 14:08:47 +0100

    update vips

--- src/popup/sections/donate.js
+++ src/popup/sections/donate.js
@@ -10,7 +10,7 @@ export default () => (
     <ListSubheader>Donate</ListSubheader>
     <ListItemText
       secondary={
-        'We develop this extension in our free time, so donations are very much appreciated, but will never be asked for. Donators who donate at least 3 Euros will get an exclusive "FACEIT Enhancer VIP" badge displayed in the match room. Please include a link to your FACEIT profile in your donation to obtain your badge. Thanks for the support.'
+        'We develop this extension in our free time, so donations are very much appreciated, but will never be asked for. Donators with a minimum of 3 Euros donation will get an exclusive "FACEIT Enhancer VIP" badge displayed in the match room. Please include a link to your FACEIT profile in your donation to obtain your badge. Thanks for the support.'
       }
     />
     <ListItemLink primary="PayPal.me" href="https://www.paypal.me/timcheung" />
