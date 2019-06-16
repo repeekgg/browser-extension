9c0055ad62f24393ddaccc8c0585e5bb4689958b
Tim Cheung <tim@cheung.io>
2019-06-16 12:14:53 +0100

    fix `git-release` script

--- scripts/git-release.js
+++ scripts/git-release.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/newline-after-import,unicorn/no-process-exit */
 const execa = require('execa')
-const semver = require('semver')
 
 ;(async () => {
   try {
@@ -9,10 +8,6 @@ const semver = require('semver')
     await gitPushMasterProc
 
     const version = await execa.stdout('git', ['log', '-1', '--pretty=%B'])
-
-    if (!semver.valid(version)) {
-      throw new Error(`Last commit is not valid version: ${version}`)
-    }
 
     const gitPushTagProc = execa('git', 'push', 'origin', `v${version}`)
     gitPushTagProc.stdout.pipe(process.stdout)
