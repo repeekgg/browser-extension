d3bb6a8fa652643d744eae65ecd059b0e5da755f
Tim Cheung <tim@cheung.io>
2019-06-18 17:21:42 +0100

    remove unused scripts

--- scripts/helpers/fetch-user.js
+++
@@ -1,9 +0,0 @@
-const got = require('got')
-
-module.exports = async nickname => {
-  const response = await got(
-    `https://api.faceit.com/core/v1/nicknames/${nickname}`,
-    { json: true }
-  )
-  return response.body.payload
-}
