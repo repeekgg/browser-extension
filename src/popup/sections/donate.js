cc4f23c9b44cc8e405dffdde97bc948b2d05b596
Tim Cheung <t.cheung@faceit.com>
2020-05-25 20:11:06 +0100

    refactor donate

--- src/popup/sections/donate.js
+++ src/popup/sections/donate.js
@@ -27,13 +27,13 @@ const VIPBadge = ({ level, bgColor, textColor }) => (
 export default () => (
   <React.Fragment>
     <ListSubheader>Donate</ListSubheader>
-    <ListItemText primary="We develop FACEIT Enhancer in our free time, so donations are very much appreciated, but will never be asked for." />
+    <ListItemText primary="FACEIT Enhancer is a free software and will always be. We develop it in our spare time, so donations are appreciated, but will never be asked for." />
     <ListItemText
       secondary={
-        'To appreciate your support, a donation of >= 3 Euros will get you an exclusive "FACEIT Enhancer VIP" badge displayed in the match room.'
+        'Optional: To appreciate your support, a donation of 3 Euros or more can get you an "FACEIT Enhancer VIP" badge displayed on the match room and on your profile.'
       }
     />
-    <ListItemText secondary="There are different badge levels based on the donation amount:" />
+    <ListItemText secondary="There are different badges based on the donation amount:" />
     {Object.keys(vipLevels)
       .map(Number)
       .map(vipLevel => (
@@ -44,18 +44,19 @@ export default () => (
             style={{ marginBottom: 2 }}
           >
             {vipLevel === 0
-              ? 'Basic: >= 3 Euros'
-              : `Level ${vipLevel}: >= ${vipLevel}0 Euros`}
+              ? 'Basic: 3 Euros or more'
+              : `Level ${vipLevel}: ${vipLevel}0 Euros or more`}
           </Typography>
           <VIPBadge level={vipLevel} {...vipLevels[vipLevel]} />
         </ListItem>
       ))}
-    <ListItemText secondary="Please include a link to your FACEIT profile in your donation to obtain your badge." />
+    <ListItemText secondary="Please include a link to your FACEIT profile in your donation if you want a badge." />
     <ListItemLink primary="PayPal.me" href="https://www.paypal.me/timcheung" />
     <ListItemLink
       primary="BuyMeACoffee.com"
       href="https://www.buymeacoffee.com/timche"
     />
     <ListItemText secondary="Thanks for your support." />
+    <ListItemText secondary="Disclaimer: Donations are non-refundable. Badges are permanent but not guaranteed to exist forever." />
   </React.Fragment>
 )
