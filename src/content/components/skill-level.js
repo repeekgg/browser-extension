0bb28120668f617332541720664cb4ead37c2fb7
Tim Cheung <t.cheung@faceit.com>
2020-06-13 19:53:37 +0100

    fix skill level size

--- src/content/components/skill-level.js
+++ src/content/components/skill-level.js
@@ -7,7 +7,7 @@ const reqSkillLevel = require.context('../assets/skill-levels', false, /\.svg$/)
 export default ({ level, size = 32, style = {} }) => (
   <span
     title={`Skill Level ${level}`}
-    style={{ width: size, ...style }}
+    style={{ width: size, display: 'inline-block', ...style }}
     dangerouslySetInnerHTML={{ __html: reqSkillLevel(`./${level}.svg`) }}
   />
 )
