7ac6a7ae2a5ddb589e575da7e84ae8dc01233542
Tim Cheung <tim@cheung.io>
2023-02-24 17:53:13 +0000

    prefix dom attribute identifier with `data-`

--- src/content/helpers/dom-element.js
+++ src/content/helpers/dom-element.js
@@ -1,4 +1,4 @@
-export const ENHANCER_ATTRIBUTE = 'faceit-enhancer'
+export const ENHANCER_ATTRIBUTE = 'data-faceit-enhancer'
 
 export const setFeatureAttribute = (featureName, element) =>
   element.setAttribute(`${ENHANCER_ATTRIBUTE}-${featureName}`, '')
