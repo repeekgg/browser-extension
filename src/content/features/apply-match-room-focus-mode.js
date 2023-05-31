8ab6fcfb2bc59ed646d55293d6d67278ebf0066c
Tim Cheung <tim@cheung.io>
2023-05-31 16:30:36 +0200

    fix match room focus mode

--- src/content/features/apply-match-room-focus-mode.js
+++ src/content/features/apply-match-room-focus-mode.js
@@ -10,8 +10,12 @@ import {
 
 const FEATURE_ATTRIBUTE = 'focus-mode'
 
-export default async parent => {
-  const parasiteRootElement = select('#parasite-container', parent).shadowRoot
+export default async () => {
+  const matchRoomOverviewElement = select('#MATCHROOM-OVERVIEW')
+
+  if (!matchRoomOverviewElement) {
+    return
+  }
 
   const roomId = getRoomId()
   const { status, teams } = await getMatch(roomId)
@@ -30,73 +34,64 @@ export default async parent => {
     return
   }
 
-  const matchRoomOverviewElement = select(
-    '#MATCHROOM-OVERVIEW',
-    parasiteRootElement
-  )
+  const teamBalanceElement =
+    matchRoomOverviewElement.children[0].children[0].children[3]
 
-  if (matchRoomOverviewElement) {
-    const teamBalanceElement =
-      matchRoomOverviewElement.children[0].children[0].children[3]
+  if (
+    teamBalanceElement &&
+    !hasFeatureAttribute(FEATURE_ATTRIBUTE, teamBalanceElement)
+  ) {
+    setFeatureAttribute(FEATURE_ATTRIBUTE, teamBalanceElement)
+    setStyle(teamBalanceElement, 'display: none')
+  }
 
+  const teamNameElements = [
+    matchRoomOverviewElement.children[0]?.children[0]?.children[1]?.children[0],
+    matchRoomOverviewElement.children[0]?.children[0]?.children[1]?.children[2]
+  ]
+
+  teamNameElements.forEach(teamNameElement => {
     if (
-      teamBalanceElement &&
-      !hasFeatureAttribute(FEATURE_ATTRIBUTE, teamBalanceElement)
+      teamNameElement &&
+      !hasFeatureAttribute(FEATURE_ATTRIBUTE, teamNameElement)
     ) {
-      setFeatureAttribute(FEATURE_ATTRIBUTE, teamBalanceElement)
-      setStyle(teamBalanceElement, 'display: none')
+      setFeatureAttribute(FEATURE_ATTRIBUTE, teamNameElement)
+      setStyle(teamNameElement, 'visibility: hidden')
     }
+  })
 
-    const teamNameElements = [
-      matchRoomOverviewElement.children[0]?.children[0]?.children[1]
-        ?.children[0],
-      matchRoomOverviewElement.children[0]?.children[0]?.children[1]
-        ?.children[2]
-    ]
+  const teamElements = [
+    select('[name="roster1"]', matchRoomOverviewElement),
+    select('[name="roster2"]', matchRoomOverviewElement)
+  ]
 
-    teamNameElements.forEach(teamNameElement => {
-      if (
-        teamNameElement &&
-        !hasFeatureAttribute(FEATURE_ATTRIBUTE, teamNameElement)
-      ) {
-        setFeatureAttribute(FEATURE_ATTRIBUTE, teamNameElement)
-        setStyle(teamNameElement, 'visibility: hidden')
-      }
-    })
+  teamElements.forEach(teamElement => {
+    if (teamElement && !hasFeatureAttribute(FEATURE_ATTRIBUTE, teamElement)) {
+      setFeatureAttribute(FEATURE_ATTRIBUTE, teamElement)
+      setStyle(teamElement, 'visibility: hidden')
+    }
+  })
 
-    const teamElements = [
-      select('[name="roster1"]', matchRoomOverviewElement),
-      select('[name="roster2"]', matchRoomOverviewElement)
-    ]
+  if (!hasFeatureAttribute(FEATURE_ATTRIBUTE, matchRoomOverviewElement)) {
+    setFeatureAttribute(FEATURE_ATTRIBUTE, matchRoomOverviewElement)
 
-    teamElements.forEach(teamElement => {
-      if (teamElement && !hasFeatureAttribute(FEATURE_ATTRIBUTE, teamElement)) {
-        setFeatureAttribute(FEATURE_ATTRIBUTE, teamElement)
-        setStyle(teamElement, 'visibility: hidden')
-      }
-    })
-
-    if (!hasFeatureAttribute(FEATURE_ATTRIBUTE, matchRoomOverviewElement)) {
-      setFeatureAttribute(FEATURE_ATTRIBUTE, matchRoomOverviewElement)
-
-      matchRoomOverviewElement.append(
-        <div
-          style={{
-            position: 'absolute',
-            bottom: 8,
-            left: 0,
-            right: 0,
-            display: 'flex',
-            justifyContent: 'center',
-            fontSize: 12,
-            color: 'rgba(255, 255, 255, 0.6)'
-          }}
-        >
-          <div style={{ background: '#1f1f1f', borderRadius: 4, padding: 8 }}>
-            Focus mode powered by Repeek
-          </div>
+    matchRoomOverviewElement.append(
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 8,
+          left: 0,
+          right: 0,
+          display: 'flex',
+          justifyContent: 'center',
+          fontSize: 12,
+          color: 'rgba(255, 255, 255, 0.6)'
+        }}
+      >
+        <div style={{ background: '#1f1f1f', borderRadius: 4, padding: 8 }}>
+          Repeek Focus Mode Active
         </div>
-      )
-    }
+      </div>
+    )
   }
 }
