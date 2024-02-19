a5dd073aa952f06b939f97c9096e30a774df050b
Tim Cheung <tim@cheung.io>
2024-02-19 17:28:19 +0000

    convert ava setup file to ts

--- ava.setup.ts
+++ ava.setup.ts
@@ -1,7 +1,9 @@
 import chrome from 'sinon-chrome'
 
+// @ts-expect-error
 global.location = new URL('https://www.faceit.com')
 
+// @ts-expect-error
 global.chrome = chrome
 
 global.chrome.runtime.id = 'testid'
